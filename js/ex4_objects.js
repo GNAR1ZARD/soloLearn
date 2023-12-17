a99db4afa3ca51e4b692e3425e69fc48521b1c99
Gnarizard <toefoo@protonmail.com>
2023-12-16 17:21:02 -0800

    almost done with js

--- js/ex4_objects.js
+++ js/ex4_objects.js
@@ -7,4 +7,21 @@
 // The given code declares two objects and calls their print() methods. 
 // Complete the code by defining the print() method for the objects.
 
-Object 
+// object constructor function
+// the 'this' keyword refers to the current object
+function contact(name, number) {
+    this.name = name;
+    this.number = number;
+    this.print = print;
+}
+
+// print function 
+function print() {
+    console.log(this.name + ": " + this.number);
+}
+
+// object instantiation
+var a = new contact("David", 12345);
+var b = new contact("Amy", 987654321)
+a.print();
+b.print();
